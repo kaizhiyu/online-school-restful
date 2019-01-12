8457f7b11487a43cfe62e92f2ae3bc1253769ced
zhulianzhai <zhulianzhai@sina.com>
2019-01-12 15:24:36 +0800

    2

--- src/main/java/com/liuxu/online/Test.java
+++ src/main/java/com/liuxu/online/Test.java
@@ -2,9 +2,8 @@ package com.liuxu.online;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("²âÊÔ");
 		B b= new B();
-		b.setA("ÂíÎä");
+		b.setA("ÂíÎä11");
 	}
 
 }
